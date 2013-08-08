4dfa7fd1e4bbd9ee2c7e93a55416ccd8cddd3bf2
Russell Joyce <russell@russellj.co.uk>
2013-08-08 15:22:04 +0100

    Reduced the number of US samples from 400 to 200 Quicker to sample and still sufficient for small distances

--- ultrasound_fpga/workspace/ultrasound/src/usarray.h
+++ ultrasound_fpga/workspace/ultrasound/src/usarray.h
@@ -6,7 +6,7 @@
 #define US_SENSOR_COUNT 10 // Number of sensors installed on platform
 #define US_SENSOR_MAP {9, 10, 11, 1, 2, 3, 4, 5, 6, 8} //Map sensor positions to sensor addresses
 #define US_SAMPLE_RATE 80000 // Hz
-#define US_RX_COUNT 400 // Number of waveform samples to take at US_SAMPLE_RATE in a single ranging operation
+#define US_RX_COUNT 200 // Number of waveform samples to take at US_SAMPLE_RATE in a single ranging operation
 #define US_TX_COUNT 8 // Cycles of 40Khz ultrasound to transmit
 
 #define USADCPrecision 10 // Bits
